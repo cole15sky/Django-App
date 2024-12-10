5637e96290124237852c51e2fef34ea2c4f86ed7
Sky Blue <dawadiaakash@gmail.com>
2024-12-10 14:24:30 +0545

    fix: app views and urls

--- myApp/myApp/settings.py
+++ myApp/myApp/settings.py
@@ -129,5 +129,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = [os.path.join(BASE_DIR,'media')]
 
-MEDIA_URL = 'static/'
+STATIC_URL = 'static/'
 STATICFILES_DIR = [os.path.join(BASE_DIR,'static')]
--- myApp/myApp/urls.py
+++ myApp/myApp/urls.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
-from django.urls import path
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.http import HttpResponse
+
+
+
+def homepage(request):
+    return HttpResponse("Welcome to the homepage!")
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+     path('', homepage, name='home'),  
+    path('peep/', include('peep.urls')),
+] +static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
